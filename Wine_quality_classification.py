2d3d0d9ca3d0706d99dd988671c6c542a5709f65
scfengv <scvfeng27@gmail.com>
2024-07-03 15:42:26 +0800

    Update result 0703

--- Wine_quality_classification.py
+++ Wine_quality_classification.py
@@ -1,10 +1,12 @@
 import os
 import gc
+import time
 import warnings
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+from tqdm import tqdm
 from sklearn import metrics
 from sklearn.svm import SVC
 from xgboost import XGBClassifier
@@ -16,7 +18,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler, label_binarize
 from mlxtend.feature_selection import SequentialFeatureSelector as SFS
 from sklearn.model_selection import StratifiedKFold, GridSearchCV, train_test_split
-from sklearn.metrics import classification_report, roc_curve, auc, ConfusionMatrixDisplay
+from sklearn.metrics import classification_report, roc_curve, auc, ConfusionMatrixDisplay, accuracy_score
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis, QuadraticDiscriminantAnalysis
 
 def get_models():
@@ -26,7 +28,7 @@ def get_models():
         'K-Nearest Neighbors': KNeighborsClassifier(),
         'Linear Discriminant Analysis': LinearDiscriminantAnalysis(),
         'Quadratic Discriminant Analysis': QuadraticDiscriminantAnalysis(),
-        'Support Vector Classifier': SVC(max_iter = int(1e7), probability = True),
+        'Support Vector Classifier': SVC(max_iter = int(1e7), probability = False, verbose = 1),
         'Random Forest': RandomForestClassifier(),
         # 'XGBoost': XGBClassifier(eval_metric = 'mlogloss')
     }
@@ -68,15 +70,16 @@ class ModelTuner:
         return self.grid_search.best_params_, self.grid_search.best_score_
 
 class FeatureSelector:
-    def __init__(self, cv, model, x, y) -> None:
+    def __init__(self, cv, model, params, x, y) -> None:
         self.cv = cv
         self.model = model
+        self.params = params
         self.x = x
         self.y = y
         self.sfs = None
 
     def select_features(self):
-        self.sfs = SFS(estimator = self.model, cv = self.cv, forward = True, floating = True, k_features = "best", scoring = "accuracy", n_jobs = -1)
+        self.sfs = SFS(estimator = self.model(**self.params), cv = self.cv, forward = True, floating = True, k_features = "best", scoring = "accuracy", n_jobs = -1)
         self.sfs.fit(self.x, self.y)
         return self.sfs.k_feature_names_
 
@@ -91,8 +94,8 @@ class WineTypeModel:
     
     def train_and_evaluate(self, model, params):
         if self.name == "Support Vector Classifier":
-            print("Identify SVC")
-            cls_ = SVC(probability = True, max_iter = 10, **params)
+            # print("Identify SVC")
+            cls_ = SVC(probability = True, max_iter = int(1e7), **params)
         else:    
             cls_ = model(**params)
         cls_.fit(self.x_train, self.y_train)
@@ -154,15 +157,16 @@ def main():
     os.chdir("/Users/shenchingfeng/GitHub/ML-Wine-Type-and-Quality-Classification/")
     df = pd.read_csv("data/Wine.csv")
     kf = StratifiedKFold(n_splits = 3, shuffle = True, random_state = 2024)
-    # training_round = ["Original data", "Upsample data"]
-    training_round = ["Upsample data"]
+    training_round = ["Original data", "Upsample data"]
+    # training_round = ["Upsample data"]
 
     for r in training_round:
 
         models = get_models()
-        model_name, model_score, model_features, tuned_params = [], [], [], []
+        model_name, model_score, model_features, tuned_params, cal_time = [], [], [], [], []
 
         for name, model in models.items():
+
             print(f"Tuning Parameters {name} / {r}...")
 
             x = df.drop(columns = ["quality", "alcohol", "pH", "fixed acidity"])
@@ -186,28 +190,58 @@ def main():
             if name in ['Logistic Regression', 'K-Nearest Neighbors', 'Support Vector Classifier']:
                 x = StandardScaler().fit_transform(x)
             x_train, x_test, y_train, y_test = train_test_split(x, y, train_size = 0.75, random_state = 2024)
-            
-            param = model_params[name]
-            tuner = ModelTuner(model = model, params = param, x = x, y = y, cv = kf)
-            best_params, score = tuner.tune_model()
+
+            if name != "Support Vector Classifier":
+                param = model_params[name]
+                tuner = ModelTuner(model = model, params = param, x = x, y = y, cv = kf)
+                best_params, score = tuner.tune_model()
+
+            else:
+                best_score_svc = -1
+                best_params_svc = {}
+
+                for c in tqdm(np.logspace(-3, 3, num = 7, base = 10)):
+                    for k in tqdm(['linear', 'poly', 'rbf', 'sigmoid']):
+                        for g in tqdm(np.logspace(-3, 3, num = 7, base = 10)):
+
+                            m = SVC(C = c, kernel = k, gamma = g)
+                            m.fit(x_train, y_train)
+                            y_pred_svc = m.predict(x_test)
+                            score_svc = accuracy_score(y_test, y_pred_svc)
+                            if score_svc > best_score_svc:
+                                best_score_svc = score_svc
+                                best_params_svc["C"] = c
+                                best_params_svc["gamma"] = g
+                                best_params_svc["kernel"] = k
+
+                    gc.collect()
 
             print(f"Selecting Features {name} / {r}...")
-            selector = FeatureSelector(cv = kf, model = model, x = x, y = y)
+            if name != "Support Vector Classifier":
+                selector = FeatureSelector(cv = kf, model = type(model), params = best_params, x = x, y = y)
+            else:
+                selector = FeatureSelector(cv = kf, model = SVC(C = best_params_svc["C"], kernel = best_params_svc["kernel"], gamma = best_params_svc["gamma"], probability = False, max_iter = int(1e7)))
             best_features = selector.select_features()
+
+            start_time = time.time()
 
             print(f"Training {name} / {r}...")
             trainer = WineTypeModel(x_train = x_train, x_test = x_test, y_train = y_train, y_test = y_test, name = name, r = r)
             trainer.train_and_evaluate(model = type(model), params = best_params)
 
+            end_time = time.time()
+            total_time = end_time - start_time
+
             model_name.append(name)
             model_score.append(round(score, 4))
             model_features.append(best_features)
             tuned_params.append(best_params)
+            cal_time.append(round(total_time, 2))
 
             gc.collect()
 
         model_tune_result = pd.DataFrame({
-            "Model Name": model_name, "Model Score": model_score, "Model Features": model_features, "Model Params": tuned_params
+            "Model Name": model_name, "Model Score": model_score, "Model Features": model_features, "Model Params": tuned_params, "Training Times (sec)": cal_time
         })
         
         model_tune_result.to_csv(f"result/Wine_Quality/{r}/Result.csv", index = False)
